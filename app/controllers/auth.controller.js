1bc2c77da4f9dddb745d3c5a68cfef395e638d75
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-08-13 20:56:46 -0400

    can now update user by id

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -102,6 +102,7 @@ exports.signin = (req, res) => {
         id: user._id,
         username: user.username,
         email: user.email,
+        trips: user.trips,
         roles: authorities,
         accessToken: token
       });
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,5 +1,6 @@
 const { authJwt } = require("../middlewares");
 const controller = require("../controllers/user.controller");
+const UserModel = require("../models/user.model");
 
 module.exports = function(app) {
   app.use(function(req, res, next) {
@@ -25,4 +26,16 @@ module.exports = function(app) {
     [authJwt.verifyToken, authJwt.isAdmin],
     controller.adminBoard
   );
+
+  // update user
+  app.put("/api/user/:id", [authJwt.verifyToken], async (request, response) => {
+    try {
+        var user = await UserModel.findById(request.params.id).exec();
+        user.set(request.body);
+        var result = await user.save();
+        response.send(result);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+  });
 };
